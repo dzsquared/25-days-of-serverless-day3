d2017f7767b43aac28a16d375d039664cc12357b
Drew Skwiers-Koballa <drew.koballa@gmail.com>
2019-12-04 19:41:22 -0600

    ready to deploy

--- GithubWebhook.cs
+++ GithubWebhook.cs
@@ -7,6 +7,9 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Company.Function
 {
@@ -19,17 +22,41 @@ namespace Company.Function
         {
             log.LogInformation("GitHub webhook received");
 
-            string name = req.Query["name"];
-
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-
-            log.LogInformation(requestBody);
-
+            var fileList = new List<string>();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            // name = name ?? data?.name;
 
-            if (data) {
+            foreach( dynamic commit in data.commits ) {
+                foreach( dynamic fileAdded in commit.added ) {
+                    string fileName = (string)fileAdded;
+                    log.LogInformation(fileName.Split('.')[fileName.Split('.').Length-1]);
+                    
+                    if ( fileName.Split('.')[fileName.Split('.').Length-1] == "png" ) {
+                        fileList.Add(fileName);
+                    }
+                }
             }
+
+            string repoUrl = data.repository.html_url;
+            var db_images  = Environment.GetEnvironmentVariable("db_images");
+            if (fileList.Count > 0) {
+                using (var connection = new SqlConnection(db_images)) {
+                    string sql = @"INSERT INTO CHRISTMAS_PNG (PNG_PATH) SELECT @PNGPATH";
+                    foreach( string pngpath in fileList) {
+                        using (SqlCommand command = new SqlCommand(sql, connection))
+                        {
+                            connection.Open();
+                            var parampngpath = new SqlParameter("PNGPATH", SqlDbType.VarChar);
+                            parampngpath.Value = repoUrl+ "/raw/master/" + pngpath;
+                            command.Parameters.Add(parampngpath);
+                            
+                            var results = command.ExecuteNonQuery();
+                        }
+                    }
+                    connection.Close();
+                }
+            }
+
             return new OkObjectResult("success");
 
         }
